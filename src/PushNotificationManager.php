bfff8b5bb965774e536614b791337b9b2090a7b2
Lynh Tran <jenky.w0w@gmail.com>
2016-08-23 09:35:10 +0700

    Parse path

--- src/PushNotificationManager.php
+++ src/PushNotificationManager.php
@@ -3,6 +3,7 @@
 namespace Jenky\LaravelPushNotification;
 
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Str;
 use Jenky\LaravelPushNotification\Contracts\PushNotification;
 
 class PushNotificationManager implements PushNotification
@@ -25,21 +26,56 @@ class PushNotificationManager implements PushNotification
     /**
      * Get the push driver.
      *
-     * @param  array|string $option
+     * @param  array|string $options
      * @return \Jenky\LaravelPushNotification\NotificationPusher
      */
-    public function driver($option)
+    public function driver($options)
     {
-        $option = is_array($option) ? $option : $this->getConfig('drivers.'.$option, []);
-        $adapterClass = $this->getConfig('adapters.'.array_get($option, 'adapter'));
+        $options = is_array($options) ? $options : $this->getConfig('drivers.'.$options, []);
+        $adapterClass = $this->getConfig('adapters.'.array_get($options, 'adapter'));
 
         if (! $adapterClass || ! class_exists($adapterClass)) {
             throw new PushNotificationException('No adapter found');
         }
 
-        $adapter = new $adapterClass(array_except($option, ['environment', 'adapter']));
+        $adapter = new $adapterClass($this->getAdapterOptions($options));
 
-        return new NotificationPusher($adapter, array_get($option, 'environment'));
+        return new NotificationPusher($adapter, array_get($options, 'environment'));
+    }
+
+    /**
+     * Get the adapter options.
+     *
+     * @param  array $option
+     * @return array
+     */
+    protected function getAdapterOptions(array $options)
+    {
+        $options = array_except($options, ['environment', 'adapter']);
+        if (! empty($options['certificate'])) {
+            $options['certificate'] = $this->getPath($options['certificate']);
+        }
+
+        return $options;
+    }
+
+    /**
+     * Parse the full path with path helper function.
+     *
+     * @param  string $path
+     * @return string
+     */
+    protected function getPath($path)
+    {
+        if (Str::contains($path, ':')) {
+            // Such as resource_path:certs/mypem.pem or storage_path:certs/mypem.pem etc.
+            list($function, $path) = explode(':', $path);
+            if (! empty($function) && ! empty($path)) {
+                return $function($path);
+            }
+        }
+
+        return $path;
     }
 
     /**
